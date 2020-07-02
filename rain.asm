bedb53eb89ef716a85cca02326880fae986ed90c
supermoneylife <davejn82@gmail.com>
2020-07-02 14:18:16 +0100

    .

--- rain.asm
+++ rain.asm
@@ -1,1 +1,8 @@
-;raindrops simulation
+
+;Spectrum screen size 256 pixels wide x 192 scan lines
+;32 x 24 Characters
+
+;Raindrops simulation
+
+
+
---
+++ raindrop.asm
@@ -0,0 +1,13 @@
+; ASM data file from a ZX-Paintbrush picture with 8 x 8 pixels (= 1 x 1 characters)
+
+; line based output of pixel data:
+
+raindrop:
+db %00000000 
+db %00000110
+db %00001100 
+db %00011000
+db %00110000 
+db %01110000
+db %01110000 
+db %00000000
